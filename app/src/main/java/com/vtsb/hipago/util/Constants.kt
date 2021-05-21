9181465f283fca64c7e33e8495bc147c75b81b25
VTSB <74222767+VTSB@users.noreply.github.com>
2021-05-22 02:56:25 +0900

    feat: Added Constants

---
+++ app/src/main/java/com/vtsb/hipago/util/Constants.kt
@@ -0,0 +1,10 @@
+package com.vtsb.hipago.util
+
+object Constants {
+
+    const val BASE_HITOMI = "hitomi.la"
+    const val URL_HITOMI = "https://$BASE_HITOMI/"
+    const val URL_LTN_HITOMI = "https://ltn.$BASE_HITOMI/"
+
+
+}
