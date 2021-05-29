7e4122913cdedc4d1e03b378c7006e347c8becaa
VTSB <74222767+VTSB@users.noreply.github.com>
2021-05-29 11:39:56 +0900

    feat: Add GalNumRepo to GalBlockUseCase

--- app/src/main/java/com/vtsb/hipago/domain/usecase/GalleryBlockUseCase.kt
+++ app/src/main/java/com/vtsb/hipago/domain/usecase/GalleryBlockUseCase.kt
@@ -1,16 +1,25 @@
 package com.vtsb.hipago.domain.usecase
 
+import com.vtsb.hipago.data.datasource.remote.entity.GalleryNumber
 import com.vtsb.hipago.domain.entity.GalleryBlock
+import com.vtsb.hipago.domain.entity.NumberLoadMode
 import com.vtsb.hipago.domain.repository.GalleryBlockRepository
+import com.vtsb.hipago.domain.repository.GalleryNumberRepository
 import kotlinx.coroutines.flow.StateFlow
 import javax.inject.Inject
 
 class GalleryBlockUseCase @Inject constructor(
-    private val galleryBlockRepository: GalleryBlockRepository
+    private val galleryBlockRepository: GalleryBlockRepository,
+    private val galleryNumberRepository: GalleryNumberRepository,
 ) {
 
-    fun getGalleryBlock(id: Int, save: Boolean=true, skipDB: Boolean=false): StateFlow<GalleryBlock> {
-        return galleryBlockRepository.getGalleryBlock(id, save, skipDB)
-    }
+    fun getGalleryNumberListByPage(loadMode: NumberLoadMode, query: String, language:String, page: Int, pageSize: Int, doLoadLength: Boolean): GalleryNumber =
+        galleryNumberRepository.getNumbersByPage(loadMode, query, language, page, pageSize, doLoadLength)
+
+    fun getGalleryBlock(id: Int, save: Boolean=true, skipDB: Boolean=false): StateFlow<GalleryBlock> =
+        galleryBlockRepository.getGalleryBlock(id, save, skipDB)
+
+    fun clearGalleryNumberBuffer(query: String, language: String) =
+        galleryNumberRepository.clearBuffer(query, language)
 
 }
