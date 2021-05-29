76c24ad2d8d1705a4aa22ce7ccb4ad3cbceecf39
VTSB <74222767+VTSB@users.noreply.github.com>
2021-05-29 10:12:08 +0900

    feat: create recyclerview adapter that has own listener

---
+++ app/src/main/java/com/vtsb/hipago/presentation/view/adapter/RecyclerViewAdapter.kt
@@ -0,0 +1,43 @@
+package io.luxus.animation.presentation.view.adapter
+
+import androidx.recyclerview.widget.RecyclerView
+import io.reactivex.android.schedulers.AndroidSchedulers
+
+abstract class RecyclerViewAdapter : RecyclerView.Adapter<RecyclerView.ViewHolder>() {
+
+    private val listener: Listener = object: Listener {
+        override fun onItemChanged(position: Int) {
+            AndroidSchedulers.mainThread().scheduleDirect { notifyItemChanged(position) }
+        }
+        override fun onRangeInserted(positionStart: Int, itemCount: Int) {
+            AndroidSchedulers.mainThread().scheduleDirect { onRangeInsertedSync(positionStart, itemCount) }
+        }
+        override fun onRangeRemoved(positionStart: Int, itemCount: Int) {
+            AndroidSchedulers.mainThread().scheduleDirect { onRangeRemovedSync(positionStart, itemCount) }
+        }
+
+        override fun onRangeInsertedSync(positionStart: Int, itemCount: Int) {
+            notifyItemRangeInserted(positionStart, itemCount)
+        }
+
+        override fun onRemovedSync(position: Int) {
+            notifyItemRemoved(position)
+        }
+
+        override fun onRangeRemovedSync(positionStart: Int, itemCount: Int) {
+            notifyItemRangeRemoved(positionStart, itemCount)
+        }
+    }
+
+    fun getListener(): Listener = listener
+
+    interface Listener {
+        fun onItemChanged(position: Int)
+        fun onRangeInserted(positionStart: Int, itemCount: Int)
+        fun onRangeInsertedSync(positionStart: Int, itemCount: Int)
+        fun onRemovedSync(position: Int)
+        fun onRangeRemoved(positionStart: Int, itemCount: Int)
+        fun onRangeRemovedSync(positionStart: Int, itemCount: Int)
+    }
+
+}
