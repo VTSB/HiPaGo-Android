239d953ead52c0916b886400c666cb1b64789fbd
VTSB <74222767+VTSB@users.noreply.github.com>
2021-05-29 14:47:04 +0900

    feat: Impl hashcode() in GalleryBlock

--- app/src/main/java/com/vtsb/hipago/domain/entity/GalleryBlock.kt
+++ app/src/main/java/com/vtsb/hipago/domain/entity/GalleryBlock.kt
@@ -9,4 +9,14 @@ data class GalleryBlock(
     val date: Date,
     val tags: Map<TagType, List<String>>,
     val thumbnail: String,
-    val related: List<Int>)
+    val related: List<Int>) {
+
+    override fun equals(other: Any?): Boolean =
+        other != null && other is GalleryBlock &&
+        id == other.id && type == other.type &&
+        title == other.title
+
+    override fun hashCode(): Int =
+        (type.hashCode()) * 31 + id
+
+}
