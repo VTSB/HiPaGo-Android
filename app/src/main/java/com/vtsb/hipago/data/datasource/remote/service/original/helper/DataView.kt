aa2ec2982a34ae06a026e092d9fb928a8ca2b9d2
VTSB <74222767+VTSB@users.noreply.github.com>
2021-05-30 11:49:33 +0900

    Fix: Fix DataView

--- app/src/main/java/com/vtsb/hipago/data/datasource/remote/service/original/helper/DataView.kt
+++ app/src/main/java/com/vtsb/hipago/data/datasource/remote/service/original/helper/DataView.kt
@@ -1,25 +1,22 @@
 package com.vtsb.hipago.data.datasource.remote.service.original.helper
 
 import java.nio.ByteBuffer
+import kotlin.math.abs
 
 class DataView(private val buffer: ByteArray) {
 
-    fun getUint8(pos: Int): Int {
-        // todo : add littleEndian & bigEndian support
-        return if (buffer[pos] < 0) -buffer[pos] else buffer[pos].toInt()
-    }
+    fun getUint8(pos: Int): Int =
+        abs(buffer[pos].toInt())
 
     fun getInt32(pos: Int): Int {
-        // todo : add littleEndian & bigEndian support
         var result = 0
         for (i in 0..4) {
-            result = result or (buffer[pos+i].toInt() shl 8 * i)
+            result = result or (getUint8(pos + i) shl 8 * (3-i))
         }
         return result
     }
 
     fun getUint64(pos: Int): Long {
-        // todo : add littleEndian & bigEndian support
         val data = ByteArray(8)
         System.arraycopy(buffer, pos, data, 0, 8)
         return ByteBuffer.wrap(data).long
