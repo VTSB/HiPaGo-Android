22bb140af7574b52077439daf468244852c2d5c1
VTSB <74222767+VTSB@users.noreply.github.com>
2021-05-30 09:35:45 +0900

    feat: Create & Impl SearchRepository

---
+++ app/src/main/java/com/vtsb/hipago/data/repository/SearchRepositoryImpl.kt
@@ -0,0 +1,44 @@
+package com.vtsb.hipago.data.repository
+
+import com.vtsb.hipago.data.mapper.GalleryDataServiceMapper
+import com.vtsb.hipago.data.mapper.TagDaoMapper
+import com.vtsb.hipago.domain.entity.Suggestion
+import com.vtsb.hipago.domain.repository.SearchRepository
+import com.vtsb.hipago.util.converter.QueryConverter
+import com.vtsb.hipago.util.converter.TagConverter
+import java.io.IOException
+import java.security.NoSuchAlgorithmException
+import java.util.*
+import javax.inject.Inject
+import javax.inject.Singleton
+
+@Singleton
+class SearchRepositoryImpl @Inject constructor(
+    private val tagDaoMapper: TagDaoMapper,
+    private val galleryDataServiceMapper: GalleryDataServiceMapper,
+    private val queryConverter: QueryConverter,
+    private val tagConverter: TagConverter,
+) : SearchRepository {
+
+
+    override fun getSuggestionList(text: String): List<Suggestion>? {
+        val query: String = queryConverter.getLastQuery(text)
+
+
+
+        return try {
+            galleryDataServiceMapper.getSuggestionForQuery(query)
+        } catch (e: InterruptedException) {
+            e.printStackTrace()
+            null
+        } catch (e: IOException) {
+            e.printStackTrace()
+            null
+        } catch (e: NoSuchAlgorithmException) {
+            e.printStackTrace()
+            null
+        }
+
+    }
+
+}
---
+++ app/src/main/java/com/vtsb/hipago/domain/repository/SearchRepository.kt
@@ -0,0 +1,9 @@
+package com.vtsb.hipago.domain.repository
+
+import com.vtsb.hipago.domain.entity.Suggestion
+
+interface SearchRepository {
+
+    fun getSuggestionList(query: String): List<Suggestion>?
+
+}
