9a7ddaa3cff415b58ce3910e085f08b3737005af
VTSB <74222767+VTSB@users.noreply.github.com>
2021-06-01 13:39:37 +0900

    Fix: Make GalleryBlock Parceable

--- app/src/main/java/com/vtsb/hipago/domain/entity/GalleryBlock.kt
+++ app/src/main/java/com/vtsb/hipago/domain/entity/GalleryBlock.kt
@@ -1,7 +1,14 @@
 package com.vtsb.hipago.domain.entity
 
+import android.os.Parcel
+import android.os.Parcelable
+import kotlinx.parcelize.Parceler
+import kotlinx.parcelize.Parcelize
 import java.sql.Date
+import java.util.*
+import kotlin.collections.ArrayList
 
+@Parcelize
 data class GalleryBlock(
     val id: Int,
     val type: GalleryBlockType,
@@ -9,7 +16,48 @@ data class GalleryBlock(
     val date: Date,
     val tags: Map<TagType, List<String>>,
     val thumbnail: String,
-    val related: List<Int>) {
+    val related: List<Int>
+): Parcelable {
+
+    private companion object : Parceler<GalleryBlock> {
+
+        override fun GalleryBlock.write(parcel: Parcel, flags: Int) {
+            parcel.writeInt(id)
+            parcel.writeSerializable(type)
+            parcel.writeString(title)
+            parcel.writeSerializable(date)
+
+            parcel.writeInt(tags.size)
+            for ((key, list) in tags.entries) {
+                parcel.writeSerializable(key)
+                parcel.writeList(list)
+            }
+            parcel.writeString(thumbnail)
+            parcel.writeList(related)
+        }
+
+        override fun create(parcel: Parcel): GalleryBlock =
+            GalleryBlock(parcel.readInt(), parcel.readSerializable() as GalleryBlockType,
+                parcel.readString()!!, parcel.readSerializable() as Date,
+                readMap(parcel), parcel.readString()!!,
+                readList(parcel, Int::class.java.classLoader!!))
+
+        private fun <T> readList(parcel: Parcel, classLoader: ClassLoader): List<T> {
+            val list: MutableList<T> = ArrayList()
+            parcel.readList(list, classLoader)
+            return list
+        }
+
+        private fun readMap(parcel: Parcel): Map<TagType, List<String>> {
+            val map: MutableMap<TagType, List<String>> = EnumMap(TagType::class.java)
+            val size = parcel.readInt()
+            for (i in 0..size) {
+                val key: TagType = parcel.readSerializable() as TagType
+                map[key] = readList(parcel, String::class.java.classLoader!!)
+            }
+            return map
+        }
+    }
 
     override fun equals(other: Any?): Boolean =
         other != null && other is GalleryBlock &&
@@ -20,3 +68,5 @@ data class GalleryBlock(
         (type.hashCode()) * 31 + id
 
 }
+
+
