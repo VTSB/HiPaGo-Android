fe033d908d963e05ff7f75af70abe8f77e88c4bf
VTSB <74222767+VTSB@users.noreply.github.com>
2021-06-01 10:28:09 +0900

    feat: Impl GalleryDetail Fragment, ViewModel

--- app/src/main/java/com/vtsb/hipago/presentation/view/fragment/GalleryDetailFragment.kt
+++ app/src/main/java/com/vtsb/hipago/presentation/view/fragment/GalleryDetailFragment.kt
@@ -1,15 +1,17 @@
 package com.vtsb.hipago.presentation.view.fragment
 
 import android.os.Bundle
-import android.view.LayoutInflater
-import android.view.View
-import android.view.ViewGroup
+import android.view.*
+import androidx.databinding.DataBindingUtil
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
+import com.vtsb.hipago.R
 import com.vtsb.hipago.databinding.FragmentGalleryDetailBinding
+import com.vtsb.hipago.presentation.view.MainActivity
 import com.vtsb.hipago.presentation.viewmodel.GalleryDetailViewModel
 import com.vtsb.hipago.util.converter.TagConverter
 import dagger.hilt.android.AndroidEntryPoint
+import java.util.*
 import javax.inject.Inject
 
 @AndroidEntryPoint
@@ -20,18 +22,45 @@ class GalleryDetailFragment : Fragment() {
     @Inject lateinit var tagConverter: TagConverter
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
-        val binding = FragmentGalleryDetailBinding.inflate(inflater, container, false)
+        val binding = DataBindingUtil.inflate<FragmentGalleryDetailBinding>(inflater, R.layout.fragment_gallery_detail, container, false)
+
 
         val galleryInformationFragmentArgs = GalleryDetailFragmentArgs.fromBundle(arguments)
-
         val galleryBlock = galleryInformationFragmentArgs.galleryBlock
 
+        viewModel.init(galleryBlock)
 
+        binding.model = galleryBlock
+        binding.lifecycleOwner = viewLifecycleOwner
+        binding.read.setOnClickListener { TODO("Move to Reader Fragment") }
 
+        setHasOptionsMenu(true)
+        val activity = requireActivity() as MainActivity
+        activity.setSupportActionBar(binding.toolbar)
+        binding.toolbar.setTitle(R.string.detail_view)
+        activity.supportActionBar?.setDisplayHomeAsUpEnabled(true)
 
-
-
-        return super.onCreateView(inflater, container, savedInstanceState)
+        return binding.root
     }
+
+    override fun onCreateOptionsMenu(menu: Menu, inflater: MenuInflater) {
+        inflater.inflate(R.menu.gallery_info, menu)
+        super.onCreateOptionsMenu(menu, inflater)
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        val id = item.itemId
+        if (id == android.R.id.home) {
+            requireActivity().onBackPressed()
+        }
+        return false
+    }
+
+
+
+
+
+
+
 
 }
--- app/src/main/java/com/vtsb/hipago/presentation/viewmodel/GalleryDetailViewModel.kt
+++ app/src/main/java/com/vtsb/hipago/presentation/viewmodel/GalleryDetailViewModel.kt
@@ -1,9 +1,13 @@
 package com.vtsb.hipago.presentation.viewmodel
 
+import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import com.vtsb.hipago.domain.entity.GalleryBlock
+import com.vtsb.hipago.domain.entity.GalleryImages
 import com.vtsb.hipago.domain.usecase.GalleryBlockUseCase
 import com.vtsb.hipago.domain.usecase.ImageUseCase
 import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.flow.SharedFlow
 import javax.inject.Inject
 
 @HiltViewModel
@@ -12,7 +16,15 @@ class GalleryDetailViewModel @Inject constructor(
     private val imageUseCase: ImageUseCase,
 ) : ViewModel() {
 
+    private val relatedListMutableLiveData = MutableLiveData<List<GalleryBlock>>()
+    private lateinit var galleryBlock: GalleryBlock
+    private lateinit var images: SharedFlow<GalleryImages>
 
+    fun init(galleryBlock: GalleryBlock) {
+        this.galleryBlock = galleryBlock
+        this.images = imageUseCase.loadList(galleryBlock.id)
+    }
 
+    fun getRelated()
 
 }
